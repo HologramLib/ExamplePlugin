c1151a8c7d5bc407e0f848af4069a5069d470e8b
Maxim <maximdeclient@gmail.com>
2024-06-22 15:17:44 +0200

    Improved commands

--- src/main/java/com/max1mde/examplehologramplugin/ExampleHologramPlugin.java
+++ src/main/java/com/max1mde/examplehologramplugin/ExampleHologramPlugin.java
@@ -27,44 +27,48 @@ public final class ExampleHologramPlugin extends JavaPlugin implements Listener 
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
-        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
-            public void run() {
-                if(!event.getMessage().startsWith("!")) return;
-                onTestCommand(event.getMessage().toLowerCase().replace("!",""), event.getPlayer());
-            }
-        });
+        if(!event.getMessage().startsWith("!")) return;
+        event.setCancelled(
+                onTestCommand(event.getMessage().toLowerCase().replace("!",""), event.getPlayer(), event))
+        ;
     }
 
-    private void onTestCommand(String command, Player player) {
+    private boolean onTestCommand(String command, Player player, AsyncPlayerChatEvent event) {
+        boolean isCommand = true;
         switch (command) {
             case "spawn" -> {
                 if(hologram != null && !hologram.isDead()) hologram.kill();
-                hologram = new TextHologram("test_hologram")
-                        .setText(ChatColor.AQUA + "Hello world!")
+                hologram = new TextHologram("test")
+                        .setMiniMessageText("<aqua>Hello world!")
+                        .setSeeThroughBlocks(false)
                         .setBillboard(Display.Billboard.VERTICAL)
                         .setShadow(true)
-                        .setScale(2,2,2)
+                        .setScale(1.5F,1.5F,1.5F)
                         .setTextOpacity((byte) 200)
-                        .setBackgroundColor(Color.fromARGB(0, 255, 236, 222).asARGB());
+                        .setBackgroundColor(Color.fromARGB(20, 255, 236, 222).asARGB());
                 HologramAPI.getHologram().spawn(hologram, player.getLocation());
-                player.sendMessage("spawned");
             }
             case "kill" -> {
                 hologram.kill();
             }
             case "animate" -> {
                 TextAnimation animation = new TextAnimation()
-                        .addFrame( ChatColor.RED + "First frame")
-                        .addFrame("Second frame")
-                        .addFrame("Third frame\nSecond line")
+                        .addFrame( "<red>First frame")
+                        .addFrame("<green>Second frame")
+                        .addFrame("Third frame\n" +
+                                "Second line in third frame")
                         .addFrame("Last frame");
+                animation.setDelay(20L); // 1 second
+                animation.setDelay(20L * 2);
                 HologramAPI.getHologram().applyAnimation(this.hologram, animation);
             }
             case "stopanimation" -> {
                 HologramAPI.getHologram().cancelAnimation(this.hologram);
+                hologram.setMiniMessageText("<aqua>Hello world!");
+                hologram.update();
             }
             case "killall" -> {
-                HologramAPI.getHologram().remove("test_hologram");
+                HologramAPI.getHologram().remove("test");
             }
             case "bigger" -> {
                 hologram.setScale(new Vector3F(5,5,5));
@@ -78,7 +82,10 @@ public final class ExampleHologramPlugin extends JavaPlugin implements Listener 
                 hologram.setScale(new Vector3F(1.5F,1.5F,1.5F));
                 hologram.update();
             }
+            default -> isCommand = false;
         }
+        if(isCommand) player.sendMessage(ChatColor.GREEN + "Success!");
+        return isCommand;
     }
 
 
